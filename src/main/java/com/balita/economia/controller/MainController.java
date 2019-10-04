29eaa4baac9dfd13c24924cbbe4b91f95fa9dd2e
Rico Flavien FAUCHARD ANDRIANAIVO <randrianaivo@dsogroup.com>
2019-10-04 16:18:12 +0400

    setup postgres

--- src/main/java/com/balita/economia/controller/MainController.java
+++ src/main/java/com/balita/economia/controller/MainController.java
@@ -1,14 +1,27 @@
 package com.balita.economia.controller;
 
+import com.balita.economia.model.Person;
+import com.balita.economia.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class MainController {
 
+    private final PersonService personService;
+
+    @Autowired
+    public MainController(PersonService personService) {
+        this.personService = personService;
+    }
+
     @RequestMapping("/")
     public String index(Model model){
+        List<Person> personList = personService.getPersons();
         return "main/index";
     }
 }
---
+++ src/main/java/com/balita/economia/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.balita.economia.repository;
+
+import com.balita.economia.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
